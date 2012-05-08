df8a5f94236ccd177daf3649f4936881150359d2
lucifer <fortran95@126.com>
2012-05-08 18:52:05 +0800

    e.py framework set up.

--- e.py
+++ e.py
@@ -5,13 +5,19 @@ from optparse import OptionParser,OptionGroup
 op = OptionParser()
 
 op.add_option("-i","--input",action="store",dest="input",help="Set the input file to be handled.")
+op.add_option('-n','--new',action='store_true',dest='new',default=False,help='Generate a new SYMKEY when -s/--symmetric provided.')
 
 group_symmetric = OptionGroup(op,'Symmetric Cipher Services','Provides symmetric encrypting and decrypting services.')
-group_symmetric.add_option('-s','--symmetric',action='store_true',dest='symmetric',help='Tell me you are going to use functions under this service group.')
-
+group_symmetric.add_option('-s','--symmetric',action='store_true',dest='symmetric',default=False,help='Tell me you are going to use functions under this service group.')
 
 op.add_option_group(group_symmetric)
 
 (options,args) = op.parse_args()
 
-#print options
+# Read verbose.
+if options.symmetric == True:
+    print "Will do symmetric jobs."
+    if options.new == True:
+        print "Will generate a new peerkey."
+else:
+    print "No verbose options received, not knowing what to do.\nYou may ask for help by adding --help option."
